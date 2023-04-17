0cee7dd2fb5170ac4b746cb8d695bf7c3af5a44a
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-17 14:45:14 +0200

    Initial commit

---
+++ crowd_human.py
@@ -0,0 +1,67 @@
+import json
+import os
+
+from PIL import Image
+
+
+def load_file(path):
+    assert os.path.exists(path)
+
+    with open(path, 'r') as file:
+        lines = file.readlines()
+    records = [json.loads(line.strip('\n')) for line in lines]
+    return records
+
+
+def crowdhuman2coco(odgt_path, json_path):
+    records = load_file(odgt_path)
+
+    json_dict = {"images": [], "annotations": [], "categories": []}
+    START_B_BOX_ID = 1
+    image_id = 1
+    bbox_id = START_B_BOX_ID
+    image = {}
+    annotations = {}
+    categories = {}
+    record_list = len(records)
+    print(record_list)
+
+    for i in range(record_list):
+        file_name = records[i]['ID'] + '.jpg'
+        img = Image.open("" + file_name)
+        image = {'file_name': file_name, 'height': img.size[1], 'widht': img.size[0], 'id': image_id}
+        json_dict['images'].append(image)
+
+        gt_box = records[i]['gt_boxes']
+        gt_box_len = len(gt_box)
+        for j in range(gt_box_len):
+            category = gt_box[j]['tag']
+            if category not in categories:
+                new_id = len(categories) + 1
+                categories[category] = new_id
+            category_id = categories[category]
+            fbox = gt_box[j]['fbox']
+
+            ignore = 0
+            if "ignore" in gt_box[j]['head_attr']:
+                ignore = gt_box[j]['head_attr']['ignore']
+            if "ignore" in gt_box[j]['extra']:
+                ignore = gt_box[j]['extra']['ignore']
+            annotations = {'area': fbox[2] * fbox[3], 'is_crowd': ignore, 'image_id': image_id, 'bbox': fbox,
+                           'hbox': gt_box[j]['hbox'], 'vbox': gt_box[j]['vbox'], 'category_id': category_id,
+                           'id': bbox_id, 'ignore': ignore, 'segmentation': []}
+            json_dict['annotations'].append(annotations)
+
+            bbox_id += 1
+        image_id += 1
+
+
+    for cate, cid in categories.items():
+        cat = {'supercategory': 'none', 'id': cid, 'name': cate}
+        json_dict['categories'].append(cat)
+
+    json_fp = open(json_path, 'w')
+    json_str = json.dumps(json_dict)
+    json_fp.write(json_str)
+    json_fp.close()
+
