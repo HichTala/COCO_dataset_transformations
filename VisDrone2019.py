d736a4f05ff1658a0c14299ad32bd7a13a6ffdd3
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-19 10:21:13 +0200

    clean up and add better path managment

--- VisDrone2019.py
+++ VisDrone2019.py
@@ -9,32 +9,21 @@ def parse_command_line():
     parser = argparse.ArgumentParser('parser', add_help=False)
 
     parser.add_argument('root', default='', type=str)
-    parser.add_argument('json_path', default='', type=str)
 
     return parser.parse_args()
 
 
-def load_file(path):
-    assert os.path.exists(path)
-
-    with open(name + ".txt", 'r') as f:
-        txt = f.read().split('\n')[:-1]
-        txt = [list(map(int, t.split(','))) for t in txt]
-    records = [json.loads(line.strip('\n')) for line in txt]
-    return records
-
-
-def visdrone2coco(root, json_path):
+def visdrone2coco(root):
     json_dict = {"images": [], "annotations": [], "categories": []}
     image_id = 1
     bbox_id = 1
     categories = ['ignore', 'pedestrian', 'person', 'bicycle', 'car', 'van', 'truck', 'awning-tricycle', 'tricycle',
                   'bus', 'motor', 'other']
 
-    type = ['train', 'val', 'test-dev']
-    for type in types:
-        image_dir = os.path.join(root, f'VisDrone2019-DET-{type}/images')
-        anno_dir = os.path.join(root, f'VisDrone2019-DET-{type}/annotations')
+    directories = ['train', 'val', 'test-dev']
+    for directory in directories:
+        image_dir = os.path.join(root, f'VisDrone2019-DET-{directory}/images')
+        anno_dir = os.path.join(root, f'VisDrone2019-DET-{directory}/annotations')
 
         file_names = sorted(os.listdir(image_dir))
         for file_name in file_names:
@@ -72,12 +61,24 @@ def visdrone2coco(root, json_path):
                     "name": category
                 })
 
-        json_file = open(json_path, 'w')
+        if directory == 'test-dev':
+            directory = directory[:4]
+        anno_coco_path = os.path.join(root, f'annotations/instances_{directory}2017.json')
+        image_coco_path = os.path.join(root, f'{directory}2017/')
+
+        if not os.path.exists(anno_coco_path):
+            os.mkdir(anno_coco_path)
+        if not os.path.exists(image_coco_path):
+            os.mkdir(image_coco_path)
+
+        json_file = open(anno_coco_path, 'w')
         json_str = json.dumps(json_dict)
         json_file.write(json_str)
         json_file.close()
 
+        os.rename(image_dir, image_coco_path)
+
 
 if __name__ == '__main__':
     args = parse_command_line()
-    visdrone2coco(args.root, args.json_path)
+    visdrone2coco(args.root)
