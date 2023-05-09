3ed86142f7c4c972d92882978c70c95b28972e25
HichTala <98521878+HichTala@users.noreply.github.com>
2023-05-09 09:31:41 +0200

    Add saving module for each dataset

--- dataset_comparison.py
+++ dataset_comparison.py
@@ -1,42 +1,62 @@
+import argparse
+import os
+import pickle
+
 import torch
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import get_cfg
 from detectron2.data import build_detection_train_loader
 
 from resnet import ResNet
-from super_pycocotools.coco import SuperCOCO
+from super_pycocotools.detectron import register
 
 
-def main():
-    super_ann_file = './datasets.json'
-    coco = SuperCOCO(super_ann_file)
+def parse_command_line():
+    parser = argparse.ArgumentParser('parser', add_help=False)
 
-    # data_dir = coco.SIXray.infos['root']
-    # data_type = 'train2017'
-    # img_dir = '{}/{}/'.format(data_dir, data_type)
+    parser.add_argument('annotations', default='', type=str)
+    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--save-path', default='./', type=str)
 
-    cfg = get_cfg()
-    cfg.DATASETS.TRAIN = coco.SIXray.register()[0]
-    cfg.SOLVER.IMS_PER_BATCH = 2
+    return parser.parse_args()
 
-    dataloader = build_detection_train_loader(cfg)
-    resnet = ResNet(cfg).cuda()
 
-    cfg.MODEL.WEIGHTS = "https://dl.fbaipublicfiles.com/detectron2/COCO-Detection/faster_rcnn_R_50_C4_1x/137257644" \
-                        "/model_final_721ade.pkl"
-    checkpointer = DetectionCheckpointer(resnet)
-    checkpointer.load(cfg.MODEL.WEIGHTS)
+def main(args):
+    datasets = register(args.annotations)
 
-    batch_mean = []
-    for data in dataloader:
-        outputs = resnet(data).mean(0).unsqueeze(0)
-        batch_mean.append(outputs)
+    batch_size = args.batch_size
 
-    batch_mean = torch.cat(batch_mean)
+    for dataset in datasets:
+        cfg = get_cfg()
+        cfg.DATASETS.TRAIN = dataset
+        cfg.SOLVER.IMS_PER_BATCH = batch_size
 
-    return batch_mean.mean(0)
+        dataloader = build_detection_train_loader(cfg)
+        resnet = ResNet(cfg).cuda()
+
+        cfg.MODEL.WEIGHTS = "https://dl.fbaipublicfiles.com/detectron2/COCO-Detection/faster_rcnn_R_50_C4_1x/137257644" \
+                            "/model_final_721ade.pkl"
+        checkpointer = DetectionCheckpointer(resnet)
+        checkpointer.load(cfg.MODEL.WEIGHTS)
+
+        batch_mean = []
+        for data in dataloader:
+            outputs = resnet(data)
+            if len(data) == batch_size:
+                outputs_mean = outputs.mean(0).unsqueeze(0)
+                batch_mean.append(outputs_mean)
+            else:
+                outputs_mean = (outputs.sum(0) / batch_size).unsqueeze(0)
+                batch_mean.append(outputs_mean)
+
+        batch_mean = torch.cat(batch_mean).mean(0)
+        save_path = os.path.join(args.save_path, dataset)
+
+        with open(save_path, 'wb') as f:
+            pickle.dump(batch_mean, f)
+            print(dataset, "ok")
 
 
 if __name__ == '__main__':
-    mean = main()
-    print(mean)
+    args = parse_command_line()
+    main(args)
