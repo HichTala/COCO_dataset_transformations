a2295393daa7853d833906d5b1993bc427499d7b
HichTala <98521878+HichTala@users.noreply.github.com>
2023-05-12 16:20:55 +0200

    First attempt

---
+++ class_comparison.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+import pickle
+
+import torch
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.data import build_detection_train_loader
+
+from resnet import ResNet
+from super_pycocotools.detectron import register
+
+
+def parse_command_line():
+    parser = argparse.ArgumentParser('parser', add_help=False)
+
+    parser.add_argument('annotations', default='', type=str)
+    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--save-path', default='./', type=str)
+
+    return parser.parse_args()
+
+
+def main(args):
+    datasets = register(args.annotations)
+
+    batch_size = args.batch_size
+
+    for dataset in datasets:
+        cfg = get_cfg()
+        cfg.DATASETS.TRAIN = dataset
+        cfg.SOLVER.IMS_PER_BATCH = batch_size
+
+        dataloader = build_detection_train_loader(cfg)
+        resnet = ResNet(cfg).cuda()
+
+        cfg.MODEL.WEIGHTS = "detectron2://backbone_cross_domain/model_final_721ade.pkl"
+        checkpointer = DetectionCheckpointer(resnet)
+        checkpointer.load(cfg.MODEL.WEIGHTS)
+
+        batch_mean = []
+        with torch.no_grad():
+            for data in dataloader:
+                outputs = resnet(data)
+                if len(data) == batch_size:
+                    outputs_mean = outputs.mean(0).unsqueeze(0)
+                    batch_mean.append(outputs_mean)
+                else:
+                    outputs_mean = (outputs.sum(0) / batch_size).unsqueeze(0)
+                    batch_mean.append(outputs_mean)
+
+            batch_mean = torch.cat(batch_mean).mean(0)
+            save_path = os.path.join(args.save_path, dataset)
+
+        with open(save_path, 'wb') as f:
+            pickle.dump(batch_mean, f)
+            print(dataset, "ok")
+
+
+if __name__ == '__main__':
+    args = parse_command_line()
+    main(args)
---
+++ roi_pool.py
@@ -0,0 +1,139 @@
+import inspect
+import logging
+from typing import List, Optional, Dict
+
+import torch
+from detectron2.config import configurable
+from detectron2.modeling import ROIHeads
+from detectron2.modeling.backbone import BottleneckBlock, ResNet
+from detectron2.modeling.poolers import ROIPooler
+from detectron2.structures import Boxes, ImageList, Instances
+from torch import nn
+
+logger = logging.getLogger(__name__)
+
+
+class Res5ROIPool(ROIHeads):
+    """
+    The ROIHeads in a typical "C4" R-CNN model, where
+    the box and mask head share the cropping and
+    the per-region feature computation by a Res5 block.
+    See :paper:`ResNet` Appendix A.
+    """
+
+    @configurable
+    def __init__(
+            self,
+            *,
+            in_features: List[str],
+            pooler: ROIPooler,
+            res5: nn.Module,
+            avgpool: nn.Module,
+            **kwargs,
+    ):
+        """
+        NOTE: this interface is experimental.
+
+        Args:
+            in_features (list[str]): list of backbone feature map names to use for
+                feature extraction
+            pooler (ROIPooler): pooler to extra region features from backbone
+            res5 (nn.Sequential): a CNN to compute per-region features, to be used by
+                ``box_predictor`` and ``mask_head``. Typically, this is a "res5"
+                block from a ResNet.
+            box_predictor (nn.Module): make box predictions from the feature.
+                Should have the same interface as :class:`FastRCNNOutputLayers`.
+            mask_head (nn.Module): transform features to make mask predictions
+        """
+        super().__init__(**kwargs)
+        self.in_features = in_features
+        self.pooler = pooler
+        if isinstance(res5, (list, tuple)):
+            res5 = nn.Sequential(*res5)
+        self.res5 = res5
+        self.avgpool = avgpool
+
+    @classmethod
+    def from_config(cls, cfg, input_shape):
+        # fmt: off
+        ret = super().from_config(cfg)
+        in_features = ret["in_features"] = cfg.MODEL.ROI_HEADS.IN_FEATURES
+        pooler_resolution = cfg.MODEL.ROI_BOX_HEAD.POOLER_RESOLUTION
+        pooler_type = cfg.MODEL.ROI_BOX_HEAD.POOLER_TYPE
+        pooler_scales = (1.0 / input_shape[in_features[0]].stride,)
+        sampling_ratio = cfg.MODEL.ROI_BOX_HEAD.POOLER_SAMPLING_RATIO
+        mask_on = cfg.MODEL.MASK_ON
+        # fmt: on
+        assert not cfg.MODEL.KEYPOINT_ON
+        assert len(in_features) == 1
+
+        ret["pooler"] = ROIPooler(
+            output_size=pooler_resolution,
+            scales=pooler_scales,
+            sampling_ratio=sampling_ratio,
+            pooler_type=pooler_type,
+        )
+
+        # Compatbility with old moco code. Might be useful.
+        # See notes in StandardROIHeads.from_config
+        if not inspect.ismethod(cls._build_res5_block):
+            logger.warning(
+                "The behavior of _build_res5_block may change. "
+                "Please do not depend on private methods."
+            )
+            cls._build_res5_block = classmethod(cls._build_res5_block)
+
+        ret["res5"], out_channels = cls._build_res5_block(cfg)
+
+        ret["avgpool"] = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        return ret
+
+    @classmethod
+    def _build_res5_block(cls, cfg):
+        # fmt: off
+        stage_channel_factor = 2 ** 3  # res5 is 8x res2
+        num_groups = cfg.MODEL.RESNETS.NUM_GROUPS
+        width_per_group = cfg.MODEL.RESNETS.WIDTH_PER_GROUP
+        bottleneck_channels = num_groups * width_per_group * stage_channel_factor
+        out_channels = cfg.MODEL.RESNETS.RES2_OUT_CHANNELS * stage_channel_factor
+        stride_in_1x1 = cfg.MODEL.RESNETS.STRIDE_IN_1X1
+        norm = cfg.MODEL.RESNETS.NORM
+        assert not cfg.MODEL.RESNETS.DEFORM_ON_PER_STAGE[-1], \
+            "Deformable conv is not yet supported in res5 head."
+        # fmt: on
+
+        blocks = ResNet.make_stage(
+            BottleneckBlock,
+            3,
+            stride_per_block=[2, 1, 1],
+            in_channels=out_channels // 2,
+            bottleneck_channels=bottleneck_channels,
+            out_channels=out_channels,
+            num_groups=num_groups,
+            norm=norm,
+            stride_in_1x1=stride_in_1x1,
+        )
+        return nn.Sequential(*blocks), out_channels
+
+    def _shared_roi_transform(self, features: List[torch.Tensor], boxes: List[Boxes]):
+        x = self.pooler(features, boxes)
+        return self.res5(x)
+
+    def forward(
+            self,
+            images: ImageList,
+            features: Dict[str, torch.Tensor],
+            targets: Optional[List[Instances]] = None,
+    ):
+        """
+        See :meth:`ROIHeads.forward`.
+        """
+        del images
+
+        target_boxes = [x.gt_boxes for x in targets]
+        box_features = self._shared_roi_transform(
+            [features[f] for f in self.in_features], target_boxes
+        )
+        box_features = self.avgpool(box_features)
+
+        return box_features
