f7cdcc0f59ebd00ab0988635ff876158fc2ce103
HichTala <98521878+HichTala@users.noreply.github.com>
2024-03-21 19:28:20 +0100

    Add diffdet pooler

--- comparisons/resnet_roi_pool.py
+++ comparisons/resnet_roi_pool.py
@@ -49,7 +49,64 @@ class ResNetROIPool(nn.Module):
         )
 
     def forward(self, batched_inputs):
+        images = self.preprocess_image(batched_inputs)
+        targets = [x["instances"].to(self.device) for x in batched_inputs]
 
+        features = self.backbone(images.tensor)
+
+        target_classes = [x.gt_classes for x in targets]
+        target_boxes = [x.gt_boxes for x in targets]
+
+        box_features = self.pooler(
+            [features[f] for f in self.in_features], target_boxes
+        )
+        box_features = self.avgpool(box_features).squeeze(dim=2).squeeze(dim=2)
+
+        return box_features, target_classes
+
+    def preprocess_image(self, batched_inputs):
+        """
+        Normalize, pad and batch the input images.
+        """
+        images = [x["image"].to(self.device) for x in batched_inputs]
+        images = [(x - self.pixel_mean) / self.pixel_std for x in images]
+        images = ImageList.from_tensors(
+            images,
+            self.backbone.size_divisibility,
+            padding_constraints=self.backbone.padding_constraints,
+        )
+
+        return images
+
+
+class DiffDetResNetROIPool(nn.Module):
+
+    def __init__(self, cfg):
+        super(DiffDetResNetROIPool, self).__init__()
+
+        self.device = torch.device(cfg.MODEL.DEVICE)
+
+        self.pixel_mean = torch.Tensor(cfg.MODEL.PIXEL_MEAN).to(self.device).view(3, 1, 1)
+        self.pixel_std = torch.Tensor(cfg.MODEL.PIXEL_STD).to(self.device).view(3, 1, 1)
+
+        self.backbone = build_backbone(cfg)
+
+        self.in_features = cfg.MODEL.ROI_HEADS.IN_FEATURES
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+
+        pooler_resolution = cfg.MODEL.ROI_BOX_HEAD.POOLER_RESOLUTION
+        pooler_scales = tuple(1.0 / self.backbone.output_shape()[k].stride for k in self.in_features)
+        sampling_ratio = cfg.MODEL.ROI_BOX_HEAD.POOLER_SAMPLING_RATIO
+        pooler_type = cfg.MODEL.ROI_BOX_HEAD.POOLER_TYPE
+
+        self.pooler = ROIPooler(
+            output_size=pooler_resolution,
+            scales=pooler_scales,
+            sampling_ratio=sampling_ratio,
+            pooler_type=pooler_type,
+        )
+
+    def forward(self, batched_inputs):
         images = self.preprocess_image(batched_inputs)
         targets = [x["instances"].to(self.device) for x in batched_inputs]
 
